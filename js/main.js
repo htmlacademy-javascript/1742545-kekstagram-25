d37ae0dd5b6a071ef2bcd8ae15e71c46d8dd69b0
Sergey <serazatyan97@gmail.com>
2022-02-28 19:34:24 +0300

    Больше деталей

--- js/main.js
+++ js/main.js
@@ -11,3 +11,32 @@ function maxStringLength(str, maxLength) {
 
 randomNumber(1, 10);
 maxStringLength('Hello World', 11);
+
+const comments = [
+'Всё отлично!',
+'В целом всё неплохо. Но не всё.',
+'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const names = ['Иван', 'Александр', 'Сергей', 'Алексей', 'Павел', 'Никита', 'Дмитрий', 'Ольга', 'Екатерина', 'Полина'];
+const descriptions = ['Море', 'Машина', 'Еда', 'Пальмы', 'Пляж'];
+let currentId = 0;
+
+function buildPhotoDescription() {
+	currentId++;
+	return {
+  id: currentId,
+  message: Array.from({length: randomNumber(1,2)}, () => comments[randomNumber(0, comments.length - 1)]).join(' '),
+  url: `photos/${currentId}.jpg`,
+  avatar: `img/avatar/${randomNumber(1, 6)}.svg`,
+  likes: randomNumber(15, 200),
+  name: names[randomNumber(0, names.length - 1)],
+  description: descriptions[randomNumber(0, descriptions.length -1)],
+  };
+}
+
+const photoDescriptions = Array.from({length: 25}, buildPhotoDescription);
+
+console.log(photoDescriptions);

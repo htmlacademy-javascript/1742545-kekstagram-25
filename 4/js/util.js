c241a926a26305601cd1e63e8127db416c9708b3
Keks <keks@htmlacademy.ru>
2022-03-02 21:23:19 +0300

    :heavy_check_mark: Сборка #4

---
+++ 4/js/util.js
@@ -0,0 +1,15 @@
+function randomNumber(from, to) {
+  if (from < 0 || to < 0 || to <= from) {
+    throw new Error('Ошибка, некорректные данные!');
+  }
+  return Math.floor(Math.random() * (to - from + 1) + from);
+}
+
+function maxStringLength(str, maxLength) {
+  return str.length <= maxLength;
+}
+
+maxStringLength('Hello World', 11);
+
+export {randomNumber};
+

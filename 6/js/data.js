5226da64c9cd3cf5a6f93a2599a0dadebaadf8d9
Keks <keks@htmlacademy.ru>
2022-03-17 16:34:33 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,37 @@
+import {randomNumber} from './util.js';
+
+const comments = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+const names = ['Иван', 'Александр', 'Сергей', 'Алексей', 'Павел', 'Никита', 'Дмитрий', 'Ольга', 'Екатерина', 'Полина'];
+const descriptions = ['Море', 'Машина', 'Еда', 'Пальмы', 'Пляж'];
+let currentId = 0;
+let commentId = 100;
+
+function createComments() {
+  return {
+    id: ++commentId,
+    avatar: `img/avatar-${randomNumber(1, 6)}.svg`,
+    message: Array.from({length: randomNumber(1, 2)}, () => comments[randomNumber(0, comments.length - 1)]).join(' '),
+    name: names[randomNumber(0, names.length - 1)],
+  };
+}
+
+function buildPhotoDescription() {
+  return {
+    id: ++currentId,
+    url: `photos/${currentId}.jpg`,
+    likes: randomNumber(15, 200),
+    description: descriptions[randomNumber(0, descriptions.length -1)],
+    comments: Array.from({length: randomNumber(0, 15)}, createComments),
+  };
+}
+
+const photoContent = Array.from({length: 25}, buildPhotoDescription);
+
+export {photoContent};
---
+++ 6/js/main.js
@@ -0,0 +1,4 @@
+import './data.js';
+import './util.js';
+import './thumbnail.js';
+import './popup.js';

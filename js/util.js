8ee8f75b9c82d69abbcf354666e9f372282ba7a2
Sergey <serazatyan97@gmail.com>
2022-03-23 13:36:03 +0300

    Исправил ошибки

--- js/util.js
+++ js/util.js
@@ -1,6 +1,6 @@
 function getRandomNumber(from, to) {
   if (from < 0 || to < 0 || to <= from) {
-    throw new Error("Ошибка, некорректные данные!");
+    throw new Error('Ошибка, некорректные данные!');
   }
   return Math.floor(Math.random() * (to - from + 1) + from);
 }
